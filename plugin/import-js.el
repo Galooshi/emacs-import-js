4d56b948ab3d1a7911b88c25d1f4c8a292a86860
Kevin Kehl <kevin.kehl@gmail.com>
2016-11-22 21:21:15 -0600

    Handle selecting multiple matches for 'word'
    It is the case in many projects that an import could resolve to two or
    more potential filepaths.
    
    On the 'word' command, handle this case and prompt the user with a
    list using grizzl, which is the base library used by fiplr, which I
    currently use to great effect, a fuzzy-find package. The interface
    is simple (using keyup/keydown) and will do the fuzzy matching for you.
    
    If your project has no such multiple matches, there should be no net
    effect, as there will not be a prompt and the import will complete.
    
    The second step after enabling the 'word' command will be enabling the
    multiple matching for 'fix', which will have to iterate over unresolved
    imports.
    
    I'm requiring the grizzl package the same way fiplr is, but I'm not
    actually familiar with dependency management in MELPA, so I'll have to
    test this out once it gets merged and MELPA picks up the change, which
    is usually a few hours.

--- plugin/import-js.el
+++ plugin/import-js.el
@@ -30,6 +30,9 @@
 
 (require 'json)
 
+(eval-when-compile
+  (require 'grizzl))
+
 (defvar import-js-buffer nil "Current import-js process buffer")
 (defvar import-buffer nil "The current buffer under operation")
 
@@ -66,7 +69,7 @@
     (skip-chars-backward "A-Za-z0-9:_")
     (let ((beg (point)) module)
       (skip-chars-forward "A-Za-z0-9:_")
-      (setq module (buffer-substring beg (point)))
+      (setq module (buffer-substring-no-properties beg (point)))
       module)))
 
 (defun import-js-write-content (import-data)
@@ -74,13 +77,43 @@
     (write-region file-content nil buffer-file-name))
   (revert-buffer t t t))
 
+(defun import-js-add (word file)
+  "Resolves an import with multiple matches"
+  (let ((json-data (json-encode-alist (list (cons word file)))))
+    (let ((import-data (json-read-from-string
+                        (import-js-send-input "add" json-data))))
+      (import-js-handle-imports import-data word))))
+
+(defun import-js-handle-unresolved (unresolved word)
+  (let ((paths (mapcar
+                (lambda (car)
+                  (cdr (assoc 'importPath car)))
+                (cdr (assoc-string word unresolved)))))
+    (let ((file (minibuffer-with-setup-hook
+                    (lambda () (make-sparse-keymap))
+                  (grizzl-completing-read (format "Unresolved import (%s)" word)
+                                          (grizzl-make-index
+                                           paths
+                                           'files
+                                           import-js-current-project-root
+                                           nil)))))
+      (import-js-add word file))))
+
+(defun import-js-handle-imports (import-data word)
+  "Check to see if import is unresolved. If resolved, write file. Else, prompt the user to select"
+  (let ((unresolved (cdr (assoc 'unresolvedImports import-data))))
+    (if unresolved
+        (import-js-handle-unresolved unresolved word)
+      (import-js-write-content import-data))))
+
 ;;;###autoload
 (defun import-js-import ()
   (interactive)
   (save-some-buffers)
-  (let ((import-data (json-read-from-string
+  (let ((word (import-js-word-at-point))
+        (import-data (json-read-from-string
                       (import-js-send-input "word" (import-js-word-at-point)))))
-    (import-js-write-content import-data)))
+    (import-js-handle-imports import-data word)))
 
 ;;;###autoload
 (defun import-js-fix ()
